8c3813b1da5d957bce677aa8d670916f11d3f811
LuizantonioDev9 <luizantoniodev9@gmail.com>
2024-10-25 08:42:49 +1300

    #

--- js/type.js
+++ js/type.js
@@ -7,13 +7,6 @@ document.addEventListener('DOMContentLoaded', () => {
         .delete(19)
         .type('em um unico ecosistema', {delay:500})
         .delete(22)
-        .go();
-    
+        .go();  
 })
-
-window.addEventListener('scroll', () => {
-    let animated = document.querySelector('.animated')
-    animated.classList.toggle('animated-disable', window.scrollY > 220)
-})
-
 
