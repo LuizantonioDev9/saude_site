33c401887a3e701374bfc8149f5b0cb6e410d876
LuizantonioDev9 <luizantoniodev9@gmail.com>
2024-11-01 14:25:22 -0300

    melhorando o codigo do menu

--- js/menuMobile.js
+++ js/menuMobile.js
@@ -3,14 +3,19 @@ const btn = document.querySelector('.btn');
 btn.addEventListener('click', () => {
     let header = document.querySelector('.header')
     let menuMobile = document.querySelector('.mobile-menu')
+    let icon = document.querySelector('.icon')
     if(menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open')
         header.classList.remove('open')
-        document.querySelector('.icon').scr = "/saude_site/assets/menuResponsivo/menu_white_36dp.svg";
+        // document.querySelector('.icon').src ="/saude_site/assets/menuResponsivo/barBlack.svg";
+        icon.src = "/saude_site/assets/menuResponsivo/barBlack.svg"; // Correção aqui
     } else {
         menuMobile.classList.add('open')
         header.classList.add('open')
-        document.querySelector('.icon').scr ="/saude_site/assets/menuResponsivo/close_white_36dp.svg";
+
+        // document.querySelector('.icon').src ="/saude_site/assets/menuResponsivo/cross.svg";
+        icon.src = "/saude_site/assets/menuResponsivo/cross.svg";
+        
     }
 })
 
