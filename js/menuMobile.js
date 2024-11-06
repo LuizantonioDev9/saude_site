17f4af29c56421d92896e748bfa3fe52a7ebd9ee
LuizantonioDev9 <luizantoniodev9@gmail.com>
2024-11-06 15:20:45 -0300

    #

--- js/menuMobile.js
+++ js/menuMobile.js
@@ -4,17 +4,16 @@ btn.addEventListener('click', () => {
     let header = document.querySelector('.header')
     let menuMobile = document.querySelector('.mobile-menu')
     let icon = document.querySelector('.icon')
+    
     if(menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open')
         header.classList.remove('open')
-        // document.querySelector('.icon').src ="/saude_site/assets/menuResponsivo/barBlack.svg";
-        icon.src = "/saude_site/assets/menuResponsivo/barBlack.svg"; // Correção aqui
-    } else {
+        icon.src = "/assets/menuResponsivo/barBlack.svg"; // Correção aqui
+    } 
+    else {
         menuMobile.classList.add('open')
         header.classList.add('open')
-
-        // document.querySelector('.icon').src ="/saude_site/assets/menuResponsivo/cross.svg";
-        icon.src = "/saude_site/assets/menuResponsivo/cross.svg";
+        icon.src = "/assets/menuResponsivo/cross.svg";
         
     }
 })
